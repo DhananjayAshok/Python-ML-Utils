613d94361ac1b36829dc7410f7ef5c6046993ede
Dhananjay Ashok <46792537+DhananjayAshok@users.noreply.github.com>
2021-02-24 21:39:34 +0530

    Create profiler.py

---
+++ profiler.py
@@ -0,0 +1,19 @@
+from time import perf_counter
+
+class Profiler:
+  def __init__(self, name=None, show=True):
+      self.name = name
+      self.show = show
+      self.t = perf_counter()
+  
+  def step(self, msg=None):
+      if not self.show:
+        return
+      interval = perf_counter() - self.t
+      if msg is not None:
+          to_print = f"{msg} | {interval}s"
+          if name is not None:
+              to_print = f"{self.name} | " + to_print
+          print(f"{msg} | {interval}s")
+      self.t = time.time()
+      return
