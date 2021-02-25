95790fb88794587b8df6e6a12b14f6d1a3b0945d
Dhananjay Ashok <46792537+DhananjayAshok@users.noreply.github.com>
2021-02-25 09:48:30 +0530

    Create printManager.py

---
+++ printManager.py
@@ -0,0 +1,16 @@
+
+class PrintManager:
+      def __init__(self, filename=None):
+          self.filename = filename
+          
+      def file_write(self, msg):
+          with open(self.filename, "a+") as f:
+              f.write(msg)
+          return
+      
+      def print(self, msg):
+          if self.filename is not None:
+              return self.file_write(msg)
+          else:
+              print(f"{msg}")
+          return
