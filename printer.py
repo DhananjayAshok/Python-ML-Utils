d0e8cfb3ef36f188dd7a41149b1d11d3f3c86adc
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2021-12-29 17:26:14 -0500

    Added print_equals and a return for everything

--- printer.py
+++ printer.py
@@ -33,9 +33,9 @@ class Printer:
             shape = obj.shape
         except:
             self.printer.print(f"Object {var_name} given to {self.name} at {meta_string} does not have a shape")
-            return
+            return None
         self.print(f"{self.name} | {var_name} | {meta_string} | {shape}")
-        return
+        return shape
     
     def print_mean_std_min_max(self, obj, var_name):
         details = inspect.getframeinfo(inspect.currentframe().f_back)
@@ -49,9 +49,9 @@ class Printer:
         except:
             self.printer.print(f"Object {var_name} given to {self.name} at {meta_string} does not have a "
                                f"mean, std, min or max")
-            return
+            return None
         self.print(f"{self.name} | {var_name} | {meta_string} | mean: {m} std: {s} min: {mi} max: {ma}")
-        return
+        return m, s, mi, ma
     
     def print_has_nans(self, obj, var_name, always_print=False):
         details = inspect.getframeinfo(inspect.currentframe().f_back)
@@ -62,7 +62,36 @@ class Printer:
         except:
             self.printer.print(f"Object {var_name} given to {self.name} at {meta_string} "
                                f"either does not have an isnan() or it's isnan() does not have any()")
-            return
+            return None
         if has_nans or always_print:
             self.print(f"{self.name} | {var_name} | {meta_string} | has nans: {has_nans}")
-        return
+        return has_nans
+
+    def print_equal(self, a, b, mean_epsilon=0.001, max_epsilon=0.001, verbose=True):
+        """
+
+        Checks if MSE(a, b) is near 0 (both the max and mean of each entry wise comparison)
+        a and b must be tensor or vector types i.e they must have a mean and a max operation as well as a - b possible
+        """
+        details = inspect.getframeinfo(inspect.currentframe().f_back)
+        meta_string = self._get_meta_string(details)
+        try:
+            diff = (a - b) ** 2
+            mean_condition = diff.mean() < mean_epsilon
+            max_condition = diff.max() < max_epsilon
+        except:
+            self.printer.print(f"Objects given to {self.name} at {meta_string} "
+                               f"either do not have an mean and max or (a-b)**2 failed")
+            return
+        if mean_condition and max_condition:
+            if verbose:
+                self.print(f"{self.name} | {meta_string} | True")
+            return True
+        else:
+            if verbose:
+                if not mean_condition:
+                    self.print(f"{self.name} | {meta_string} | "
+                               f"False: mean of (a-b)**2 is {diff.mean()} >= {mean_epsilon}")
+                if not max_condition:
+                    self.print(f"{self.name} | {meta_string} | False: max of (a-b)**2 is {diff.max()} >= {max_epsilon}")
+            return False
