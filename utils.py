8700ba4e9cb527a47d20975aa6740381cb7257f9
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2022-10-11 21:27:31 +0530

    Added pbar

--- utils.py
+++ utils.py
@@ -12,3 +12,25 @@ def safe_mkdir(path):
             return -1
 
 
+def pit(it, *pargs, **nargs):
+    import enlighten
+    global __pit_man__
+    try:
+        __pit_man__
+    except NameError:
+        __pit_man__ = enlighten.get_manager()
+    man = __pit_man__
+    try:
+        it_len = len(it)
+    except:
+        it_len = None
+    try:
+        ctr = None
+        for i, e in enumerate(it):
+            if i == 0:
+                ctr = man.counter(*pargs, **{**dict(leave = False, total = it_len), **nargs})
+            yield e
+            ctr.update()
+    finally:
+        if ctr is not None:
+            ctr.close()
